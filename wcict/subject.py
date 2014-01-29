f4d50959d94b6bf3808cfb2f3feedec98bec98c6
ramnes <contact@ramnes.eu>
2014-01-29 18:34:51 +0000

    Fake DB

--- wcict/subject.py
+++ wcict/subject.py
@@ -1,8 +1,17 @@
+from random import choice
 from flask import current_app
 
 
+language = ["C", "C++", "Python", "Java", "Whitespace", "Brainfuck"]
+
+what = ["a random coding subject generator",
+        "a {} compiler".format(choice(language)),
+        "a {} prepocessor".format(choice(language))]
+
+tech = ["Heroku", "Amazon AWS", "Emacs", "my feets"]
+
 def random_subject(app=current_app):
-    return
+    return "{} in {} with {} and {}".format(choice(what), choice(language), choice(tech), choice(tech))
 
 
 __all__ = ["random_subject"]
