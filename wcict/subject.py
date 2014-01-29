ba270e781718268e510edcb18d80a7f5baee27dd
ramnes <contact@ramnes.eu>
2014-01-29 19:51:14 +0000

    Fake DB update

--- wcict/subject.py
+++ wcict/subject.py
@@ -2,16 +2,34 @@ from random import choice
 from flask import current_app
 
 
-language = ["C", "C++", "Python", "Java", "Whitespace", "Brainfuck"]
+languages = ["C", "C-ANSI", "C++", "C#",
+             "Python", "Python2.7", "Python3",
+             "Java",
+             "Javascript",
+             "Bash", "SH", "PHP",
+             "Whitespace", "Brainfuck", "Lua", "ASM"]
 
-what = ["a random coding subject generator",
-        "a {} compiler".format(choice(language)),
-        "a {} prepocessor".format(choice(language))]
+whats = ["a random coding subject generator", "a {language} compiler",
+         "{language} bindings", "a {language}-based library wrapper",
+         "a {provider} API wrapper", "a {techno} client"]
 
-tech = ["Heroku", "Amazon AWS", "Emacs", "my feets"]
+technos = ["Heroku", "Amazon AWS", "Firebase",
+           "MongoDB", "PostgreSQL", "MySQL", "Cassandra", "MySQL", "Riak",
+           "Docker", "VirtualBox", "VMWare",
+           "CUDA", "OpenGL", "Unity",
+           "my dick", "my feets"]
+
+providers = ["Youtube", "Facebook", "DailyMotion", "Yahoo"]
+
 
 def random_subject(app=current_app):
-    return "{} in {} with {} and {}".format(choice(what), choice(language), choice(tech), choice(tech))
+    what = choice(whats)
+    what = what.format(language=choice(languages),
+                       provider=choice(providers),
+                       techno=choice(technos))
+    language = choice(languages)
+    techno = choice(technos)
+    return "{} in {} with {}".format(what, language, techno)
 
 
 __all__ = ["random_subject"]
